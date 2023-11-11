417ad023e91e66991df63bec1411e3588b52f70b
eileenh8 <eileen.henriquez@icloud.com>
2023-11-10 20:34:36 -0500

    completes

--- Levels/Exit.gd
+++ Levels/Exit.gd
@@ -5,9 +5,8 @@ extends Area2D
 
 func _on_body_entered(body):
 	var current_scene = get_tree().current_scene.scene_file_path
-	print(body.name)
 	if body.name == "Player":
-		if current_scene == "res://Levels/Level1.tscn":
+		if current_scene == "res://Levels/level1.tscn":
 			get_tree().change_scene_to_file("res://Levels/level2.tscn")
-		elif current_scene == "res://Levels/Level2.tscn":
-			get_tree().change_scene_to_file("res://Levels/level1.tscn")
+		elif current_scene == "res://Levels/level2.tscn":
+			get_tree().change_scene_to_file("res://Levels/level3.tscn")
--- Player/Player_Container.gd
+++ Player/Player_Container.gd
@@ -4,5 +4,5 @@ func _physics_process(delta):
 	if get_child_count() <= 1:
 		var Player = load("res://Player/player.tscn")
 		var player = Player.instantiate()
-		player.position = $Spawn2.position
+		player.position = $Spawn.position
 		add_child(player)
